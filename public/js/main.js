f14e7ff47db500f24536d7843b237f674ec805d0
HUGE | Timothy McDuffie <tmcduffie@hugeinc.com>
2015-02-10 08:35:42 -0500

    completely changed file to implement new ben theme funcitonality and implement CommonJS module pattern

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,66 @@
-/* jshint browser: true */
-(function() {
-  'use strict';
+/* jshint browser:true */
+'use strict';
 
-  var slides;
-  var classNames = {
-    PREV: 'previous',
-    CURR: 'current',
-    NEXT: 'next'
-  };
 
-  writeSlideCss();
-  manageSlideTransition();
+/**
+ * Nav animator
+ */
+var Nav = require('./lib/ui/nav');
 
-  // make the slides fillscreen
-  function writeSlideCss() {
-    var viewportSize = {
-      x: window.innerWidth,
-      y: window.innerHeight
-    };
-    var styleCSS = document.createElement('style');
-    styleCSS.id = 'style-css';
-    styleCSS.innerHTML = '.slide { height: ' + viewportSize.y + 'px; width: ' + viewportSize.x + 'px; }';
-    document.body.appendChild(styleCSS);
-    console.log('style rlement written');
-  }
+var nav = new Nav(document.querySelector('.header'), '.hamburger', 'nav', 'expanded');
 
-  function manageSlideTransition() {
-    slides = document.querySelectorAll('section.slide');
-    console.log(slides);
-    Array.prototype.forEach.call(slides, function(elem, index) {
-      var className = classNames.NEXT;
-      if (index === 0) {
-        className = classNames.CURR;
-      }
-      elem.classList.add(className);
-    });
-  }
 
-  // function mapSlides() {
+/**
+ * parallax scroller
+ */
+var ParallaxScroller = require('./lib/ui/ParallaxScroller');
 
-  // };
+var trigger = document.getElementById('title').querySelector('h1');
+var target = document.querySelector('.header header');
+var elemStartY = target.parentElement.offsetHeight - (target.offsetTop - target.parentElement.offsetTop);
 
-  // function handleScroll() {
-  //   document.addEventListener('scroll', function() {
+var headerParallax = new ParallaxScroller(target, {
+    elemStartY: elemStartY,
+    elemEndY: 0,
+    scrollListenerStartY: trigger.offsetTop - target.parentElement.offsetHeight,
+    scrollListenerEndY: trigger.offsetTop + trigger.offsetHeight
+});
 
-  //   });
-  // };
-}());
+headerParallax.setRecipientOffset(elemStartY);
+
+
+/**
+ * Form Validation and submission.
+ */
+var Form = require('./lib/form/Form');
+var contactFormErrors = require('./templates/contact-form-errors');
+
+var contactForm = new Form(document.querySelector('#contact form'));
+contactForm
+    .on(Form.events.HANDLE_SUBMIT, function () {
+        // clear
+        console.log('clearing errors');
+        console.dir(this.elem.previousElementSibling);
+        console.log(this.elem.previousElementSibling.tagName === 'UL' &&
+            this.elem.previousElementSibling.className === 'errors');
+        if (this.elem.previousElementSibling.tagName === 'UL' &&
+            this.elem.previousElementSibling.className === 'errors') {
+            this.elem.previousElementSibling.remove();
+        }
+
+    }.bind(contactForm))
+    .on(Form.events.VALIDATE_FAIL, function () {
+        console.log('validation failed with errors', this.errors);
+
+        // this should render a template instead of generate a string.
+        var errorsList = contactFormErrors(this.errors);
+        this.elem.parentNode.insertBefore(errorsList, this.elem);
+
+    }.bind(contactForm));
+
+
+module.exports = {
+  nav: nav,
+  header: headerParallax,
+  form: contactForm
+};
